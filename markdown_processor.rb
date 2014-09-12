ea3952f1a457c780e62e812631b9f387eea2ba66
“Jen <mpjeno@gmail.com>
2014-09-12 08:51:30 -0500

    Added documentation for convert_to_h1 method

--- markdown_processor.rb
+++ markdown_processor.rb
@@ -1,6 +1,16 @@
 require 'pry'
 module MarkdownProcessor
 
+  # Place a string inside <h1> tags ONLY IF it starts with a "#"
+  #
+  # string - the String to be manipulated
+  #
+  # Example:
+  #
+  # convert_to_h1("#hello")
+  #   => '"<h1>hello</h1>"
+  #
+  # Returns the initial string, minus the "#", wrapped in an <h1> tag
   def convert_to_h1(string)
     split_string = string.split(//)
     if split_string[0] == "#"
@@ -14,16 +24,3 @@ module MarkdownProcessor
   end
 
 end
-
-binding.pry
-
-#
-# This part is totally unrelated to Rails. It's just a Ruby file. Create it separately as its own project:
-#
-# Make a new file called markdown_processor.rb. Create a module called MarkdownProcessor and add one method to it. That method (called convert_to_h1) should take a string and do the following to it:
-#
-# If the first character of the string is "#", then that line (excluding the "#") should be wrapped in an <h1> tag. Meaning that if I do convert_to_h1("# Hello world."), the method you write should return "<h1>Hello world.</h1>".
-# If the first character of the string is not "#", the method should return false.
-# Write tests for your method.
-#
-# (Go back and consult the Rspec tutorial if you need to.)
--- spec/markdown_processor_spec.rb
+++ spec/markdown_processor_spec.rb
@@ -5,18 +5,19 @@ describe MarkdownProcessor do
 class DummyClass
 end
   
+before(:all) do
+  @dummy = DummyClass.new
+  @dummy.extend MarkdownProcessor
+end
+  
   describe "convert_to_h1" do
       
     it "should convert a string with char1 = # to an <h1> tag" do
-      @dummy = DummyClass.new
-      @dummy.extend MarkdownProcessor
       a = "#hello"
       expect(@dummy.convert_to_h1(a)).to eq("<h1>hello</h1>")
     end
     
     it "should return false if the string does not start with '#'" do
-      @dummy = DummyClass.new
-      @dummy.extend MarkdownProcessor
       a = "hello"
       expect(@dummy.convert_to_h1(a)).to eq(false)
     end
